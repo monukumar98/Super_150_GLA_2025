50d9d51e83dfb4d7111e7564c20bc01d401ff694
monukumar98 <monu012as@gmail.com>
2024-01-11 16:02:22 +0530

    TOPOlogical_Sort Qps

---
+++ Lec77/Alien_Dictionary.java
@@ -0,0 +1,10 @@
+package Lec77;
+
+public class Alien_Dictionary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lec77/Longest_Cycle_in_a_Graph.java
@@ -0,0 +1,10 @@
+package Lec77;
+
+public class Longest_Cycle_in_a_Graph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
