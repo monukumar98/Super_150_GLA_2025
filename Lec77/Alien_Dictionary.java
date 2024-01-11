87b75aec5e34111b3b8e951684081def1f7e738f
monukumar98 <monu012as@gmail.com>
2024-01-11 16:23:30 +0530

    TOPOlogical_Sort Qps

--- Lec77/Alien_Dictionary.java
+++ Lec77/Alien_Dictionary.java
@@ -1,10 +1,68 @@
 package Lec77;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Alien_Dictionary {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	class Solution {
+		public String alienOrder(String[] words) {
+			HashMap<Character, List<Character>> map = new HashMap<>();
+			for (int i = 0; i < words.length; i++) {
+				String w = words[i];
+				for (char ch : w.toCharArray()) {
+					if (!map.containsKey(ch)) {
+						map.put(ch, new ArrayList<>());
+					}
+				}
+			}
+			for (int i = 0; i < words.length - 1; i++) {
+				String s1 = words[i];
+				String s2 = words[i + 1];
+				if (s1.startsWith(s2) && s1.length() > s2.length()) {
+					return "";
+				}
+				for (int j = 0; j < Math.min(s1.length(), s2.length()); j++) {
 
+					if (s1.charAt(j) != s2.charAt(j)) {
+						map.get(s1.charAt(j)).add(s2.charAt(j));
+						break;
+					}
+				}
+			}
+			int in[] = new int[26];
+			for (char key : map.keySet()) {
+				for (char nbrs : map.get(key)) {
+					in[nbrs - 'a']++;
+				}
+			}
+			Queue<Character> q = new LinkedList<>();
+			for (char ch : map.keySet()) {
+				if (in[ch - 'a'] == 0) {
+					q.add(ch);
+
+				}
+			}
+			StringBuilder sb = new StringBuilder();
+			int c=0;
+			while (!q.isEmpty()) {
+				char ch = q.remove();
+				sb.append(ch);
+				c++;
+				for (char nbrs : map.get(ch)) {
+					in[nbrs - 'a']--;
+					if (in[nbrs - 'a'] == 0) {
+						q.add(nbrs);
+					}
+				}
+			}
+			
+			return c==map.size()?sb.toString():"";
+
+		}
 	}
 
 }
--- Lec77/Longest_Cycle_in_a_Graph.java
+++ Lec77/Longest_Cycle_in_a_Graph.java
@@ -1,10 +1,53 @@
 package Lec77;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Longest_Cycle_in_a_Graph {
+	class Solution {
+		public int longestCycle(int[] edges) {
+			int[] in = new int[edges.length];
+			for (int i = 0; i < edges.length; i++) {
+				if (edges[i] != -1) {
+					in[edges[i]]++;
+				}
+			}
+			Queue<Integer> q = new LinkedList<>();
+			for (int i = 0; i < in.length; i++) {
+				if (in[i] == 0) {
+					q.add(i);
+				}
+			}
+			int[] visited = new int[edges.length];
+			while (!q.isEmpty()) {
+				int v = q.poll();
+				visited[v] = 1;
+				int nbrs = edges[v];
+				if (nbrs != -1) {
+					in[nbrs]--;
+					if (in[nbrs] == 0) {
+						q.add(nbrs);
+					}
+				}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			}
+			int ans = -1;
+			for (int i = 0; i < visited.length; i++) {
+				if (visited[i] == 0) {
+					int c = 1;
+					visited[i] = 1;
+					int nbrs = edges[i];
+					while (nbrs != i) {
+						c++;
+						visited[nbrs] = 1;
+						nbrs = edges[nbrs];
+					}
 
+					ans = Math.max(ans, c);
+				}
+			}
+			return ans;
+		}
 	}
 
 }
